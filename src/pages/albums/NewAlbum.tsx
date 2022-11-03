db52c548fd0dbbb5eb748bdf318c0241298c8e5c
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2022-11-03 16:40:29 +0200

    Added admin button to generate data

--- src/pages/albums/NewAlbum.tsx
+++ src/pages/albums/NewAlbum.tsx
@@ -9,7 +9,6 @@ import {
   TextFieldProps,
   Typography,
 } from '@mui/material'
-
 import { LoadingButton } from '@mui/lab'
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
@@ -19,11 +18,96 @@ import moment, { Moment } from 'moment'
 
 import { APIStatus } from 'api/MainApi'
 
+import { postCreateAlbumAsync } from 'store/albums/actions' // TODO: temporary
 import { selectErrors, selectStatus } from 'store/albums/selectors'
-import { postCreateAlbumAsync } from 'store/albums/actions'
 
 import { useInput } from 'components/hooks/useInput'
 import { useDidMountEffect } from 'components/hooks/useDidMountEffect'
+import { selectLogin } from 'store/user/selectors' // TODO: temporary
+
+const mockAlbums = [
+  // TODO: temporary
+  {
+    name: 'Sundance Film Festival',
+    location: 'Park City, Utah',
+    date: '2022-01-21T08:49:52z',
+  },
+  {
+    name: 'Super Bowl Sunday',
+    location: 'Hard Rock Stadium, Miami, Florida',
+    date: '2018-10-19T08:27:32z',
+  },
+  {
+    name: 'Mardi Gras',
+    location: 'New Orleans, Louisiana',
+    date: '2021-07-23T06:05:19z',
+  },
+  {
+    name: 'Masters Golf Tournament',
+    location: 'Augusta National Gold Club, Georgia',
+    date: '2021-02-09T05:33:33z',
+  },
+  {
+    name: 'Coachella',
+    location: 'Empire Polo Club in Indio, California',
+    date: '2022-06-22T09:31:01z',
+  },
+  {
+    name: 'The Boston Marathon',
+    location: 'Boston, Massachusetts',
+    date: '2017-05-02T09:26:08z',
+  },
+  {
+    name: 'Independence Day',
+    location: 'Throughout the USA',
+    date: '2018-12-09T05:05:31z',
+  },
+  {
+    name: 'Comic-Con International',
+    location: 'San Diego, California',
+    date: '2017-02-08T06:20:34z',
+  },
+  {
+    name: 'Lollapalooza',
+    location: 'Chicago, Illinois',
+    date: '2018-07-24T08:30:28z',
+  },
+  {
+    name: 'Burning Man Festival',
+    location: 'Black Rock Desert, Nevada',
+    date: '2020-12-26T05:34:16z',
+  },
+  {
+    name: 'US Open Tennis Championships',
+    location: 'USTA Billie Jean King National Tennis Center, New York',
+    date: '2017-01-27T02:54:07z',
+  },
+  {
+    name: 'Halloween',
+    location: 'Lafayette Walk, Wheaton, Utah',
+    date: '2020-11-11T06:44:05z',
+  },
+  {
+    name: 'Thanksgiving Day',
+    location: 'Fairview Place, Nevada',
+    date: '2018-04-19T03:56:01z',
+  },
+  {
+    name: 'New Year’s Eve in Times Square',
+    location: 'Times Square, New York',
+    date: '2020-01-19T10:39:31z',
+  },
+  {
+    name: 'Jane Smith photo session on Bridge',
+    location: 'Brooklyn Bridge',
+    date: '2022-01-10T06:30:22z',
+  },
+  {
+    name: 'Jane Smith, Botanical Garden',
+    location: 'Brooklyn Botanical Garden',
+    date: '2022-05-12T06:30:22z',
+  },
+] as const
 
 const NewAlbum: FC = () => {
   const dispatch = useDispatch()
@@ -31,6 +115,7 @@ const NewAlbum: FC = () => {
 
   const status = useSelector(selectStatus)
   const errors = useSelector(selectErrors)
+  const login = useSelector(selectLogin) // TODO: temporary
 
   const [name, setName] = useInput('')
   const [location, setLocation] = useInput('')
@@ -48,7 +133,7 @@ const NewAlbum: FC = () => {
       setLocation.setState('')
       setDate(null)
       setValidation({ isValid: true, message: '' })
-      navigate(-1)
+      navigate(-1) // TODO: temporary
     }
 
     if (status === APIStatus.REJECTED) {
@@ -68,6 +153,20 @@ const NewAlbum: FC = () => {
     }
 
     await dispatch(postCreateAlbumAsync({ name, location, date: moment(date).utc().format() }))
+  }
+
+  const handleOnClickGenerate = async () => {
+    // TODO: temporary
+    for (let i = 0; i < mockAlbums.length; i++) {
+      const { name, location, date } = mockAlbums[i]
+      await dispatch(
+        postCreateAlbumAsync({
+          name: name + ' test 1',
+          location,
+          date: moment(date).utc().format(),
+        }),
+      )
+    }
   }
 
   return (
@@ -160,6 +259,25 @@ const NewAlbum: FC = () => {
             >
               Save
             </LoadingButton>
+
+            {(login === 'bkrevsun' || login === 'oleksii') && ( // TODO: temporary
+              <LoadingButton
+                loading={status === APIStatus.PENDING}
+                loadingIndicator={
+                  <CircularProgress
+                    size={18}
+                    sx={{ color: 'inherit', position: 'absolute', top: '-9px', left: '2px' }}
+                  />
+                }
+                loadingPosition='end'
+                variant='contained'
+                fullWidth
+                sx={{ borderRadius: '50px', height: '50px', marginBottom: '10px' }}
+                onClick={handleOnClickGenerate}
+              >
+                Generate
+              </LoadingButton>
+            )}
 
             {!validation.isValid && validation.message && (
               <FormHelperText error={!validation.isValid} sx={{ textAlign: 'center' }}>
